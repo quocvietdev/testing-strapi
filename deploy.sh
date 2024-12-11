dd88d0ae142b364311d556bc668cc8f04745e0c5
Quoc <quoc@bcxstudio.com>
2024-12-11 21:05:30 +0700

    update

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo "Removing src, config"
-rm -rf /home/site/wwwroot/src
-rm -rf /home/site/wwwroot/config
-rsync -arv --no-o --no-g --ignore-existing --size-only  ./ /home/site/wwwroot
-echo "Source sync done."
